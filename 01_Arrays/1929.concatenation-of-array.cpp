079cabd831ebcce82daf404bd305710e0442d92e
Suhaan Khan <suhaankhan0032@gmail.com>
2026-05-11 07:56:15 +0000

    feat: implement Concatenation of Array with O(n) runtime

---
+++ 01_Arrays/1929.concatenation-of-array.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1929 lang=cpp
+ *
+ * [1929] Concatenation of Array
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(2 * n);
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
