b73b0da0874bbe775948847b9e3d5a418acea79b
Suhaan Khan <suhaankhan0032@gmail.com>
2026-05-27 06:40:43 +0000

    feat: solved #1422 maximum score after splitting a string (3ms)

---
+++ 02_Strings/1422-maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1422 lang=cpp
+ *
+ * [1422] Maximum Score After Splitting a String
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxScore(string s) {
+        
+    }
+};
+// @lc code=end
+
