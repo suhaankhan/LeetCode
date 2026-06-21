f74522f02ec00edc6d0bb731ce17ae202d36af85
Suhaan Khan <suhaankhan0032@gmail.com>
2026-06-21 23:18:06 +0000

    feat: solved #392 is subsequence using 0ms 100% runtime

---
+++ 09_Two_Pointers/0392-is-subsequence.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=392 lang=cpp
+ *
+ * [392] Is Subsequence
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0;
+        int j = 0;
+
+        while (i < s.size() && j < t.size())
+        {
+            if (s[i] == t[j]) 
+            {
+                i++;
+            }
+            j++;
+        }
+        return i == s.size();
+    }
+};
+// @lc code=end
+
